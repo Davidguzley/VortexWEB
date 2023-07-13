f05775ce138bd7b3a0626253cf59b34d5df09fd0
Davidguzley <58478139+Davidguzley@users.noreply.github.com>
2023-07-12 20:03:58 -0600

    feat: Startup proyect

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+  return (
+    <main>
+      {/* Header */}
+      {/* Vortex */}
+      {/* About */}
+      {/* Experience */}
+      {/* skills */}
+      {/* Projects */}
+      {/* Contact Me */}
+    </main>
+  )
+}
